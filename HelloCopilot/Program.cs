45963f4fb89e8440675a52b827adfec3eb68d0b5
Em S <eshockley@microsoft.com>
2025-07-22 09:51:22 -0700

    init

---
+++ HelloCopilot/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCopilot
+{
+  public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Print out Hello, Copilot 3,000 time with incrementing index
+            for (int i = 0; i < 3000; i++)
+            {
+                Console.WriteLine($"Hello, Copilot {i}");
+            }
+
+            // Sum all the numbers in a list of integers
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine($"Sum of numbers: {Sum(numbers)}");
+            
+        }
+
+
+        
+
+        /* Function to sum all the numbers in a list of integers */
+        private static int Sum(List<int> numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+            
+
+        public static int AddTwoNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int SubtractTwoNumbers(int v1, int v2)
+        {
+            //return v1-v2;
+            return v1 - v2;
+
+        }
+
+        //function to divide two numbers
+        public static int DivideTwoNumbers(int v1, int v2)
+        {
+          
+            return v1 / v2;
+        }
+
+
+
+    }
+}
---
+++ HelloCopilot/README.md
@@ -0,0 +1,20 @@
+﻿<p>
+    &nbsp;</p>
+<p>
+    &nbsp;</p>
+<p>
+    Script</p>
+<p>
+    &nbsp;</p>
+<p>
+    /* Function to randomly assign 4 digit codes to N x M matrix representing lockers */</p>
+<p>
+    &nbsp;</p>
+<p>
+    In Main()</p>
+<p>
+    // Assign locker codes to a 2x2 matrix</p>
+<p>
+    public static int assignLockerCodes(int N, int M)</p>
+<p>
+&nbsp;{ Random random = new Random(); int[,] lockerCodes = new int[N, M]; for (int i = 0; i &lt; N; i++) { for (int j = 0; j &lt; M; j++) { lockerCodes[i, j] = random.Next(1000, 9999); } } return lockerCodes[0, 0]; }</p>
